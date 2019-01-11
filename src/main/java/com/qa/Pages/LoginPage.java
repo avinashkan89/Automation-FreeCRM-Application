20d34d7f3473d4d93cef68d5529c1dbe1bee95ea
avinashkan89 <40767515+avinashkan89@users.noreply.github.com>
2019-01-11 15:57:37 +0530

    Update LoginPage.java

--- src/main/java/com/qa/Pages/LoginPage.java
+++ src/main/java/com/qa/Pages/LoginPage.java
@@ -4,6 +4,9 @@ public class LoginPage {
 
 	public static void main(String[] args) {
 		System.out.println("Login page printed");
+		
+		// this is a sample edit
+		//import this to your eclipse projet
 
 	}
 
